3fd194d1f30fff80034864683f31d7285916c715
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-09-19 23:42:02 +0430

    For loop in template.

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -23,13 +23,17 @@ def index(request):
     list_items = ""
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f'<li><a href="{month_path}">{capitalized_month}</a></li>'
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    # for month in months:
+    #     capitalized_month = month.capitalize()
+    #     month_path = reverse("month-challenge", args=[month])
+    #     list_items += f'<li><a href="{month_path}">{capitalized_month}</a></li>'
+
+    # response_data = f"<ul>{list_items}</ul>"
+    # return HttpResponse(response_data)
 
 
 def monthly_challenge_by_number(request, month):
