feee1234cfa7259b5eaac394f404e37c91c294d9
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-09-19 23:24:49 +0430

    Template filter.

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -48,7 +48,7 @@ def monthly_challenge(request, month):
         challenge_text = monthly_challenges[month]
         return render(request, "challenges/challenge.html", {
             "text": challenge_text,
-            "month": month.capitalize()
+            "month": month
         })
         # response_data = render_to_string("challenges/challenge.html")
         # return HttpResponse(response_data)
