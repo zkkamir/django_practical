bf5b1830cdf35730926907d9948774b3a9109e5f
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-09-19 12:50:57 +0430

    Reformatted the view to be more dynamic.

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -2,18 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 
+monthly_challenges = {
+    "january": "challenge for jan",
+    "february": "challenge for feb",
+    "march": "challenge for mar",
+    "april": "challenge for apr",
+    "may": "challenge for may",
+    "june": "challenge for jun",
+    "july": "challenge for jul",
+    "august": "challenge for aug",
+    "september": "challenge for sep",
+    "october": "challenge for oct",
+    "november": "challenge for nov",
+    "december": "challenge for dec"
+}
+
+
 def monthly_challenges_by_number(request, month):
     return HttpResponse(month)
 
 
 def monthly_challenges(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "Eat more fruits!"
-    elif month == "february":
-        challenge_text = "Walk!"
-    elif month == "march":
-        challenge_text = "Read!"
-    else:
-        return HttpResponseNotFound("This month is not supported!")
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This is not a valid month!")
