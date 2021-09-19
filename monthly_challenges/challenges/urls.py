ed3535f61baac7cb6665a554be7d8d519faa9f84
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-09-19 13:02:55 +0430

    Added redirection from number to the actual month.

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:month>", views.monthly_challenges_by_number),
-    path("<str:month>", views.monthly_challenges)
+    path("<int:month>", views.monthly_challenge_by_number),
+    path("<str:month>", views.monthly_challenge)
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseNotFound
 
 
@@ -18,11 +19,17 @@ monthly_challenges = {
 }
 
 
-def monthly_challenges_by_number(request, month):
-    return HttpResponse(month)
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month!")
+
+    redirect_month = months[month - 1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
 
 
-def monthly_challenges(request, month):
+def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
         return HttpResponse(challenge_text)
