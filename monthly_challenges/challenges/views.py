7ea0e31c9463b57b5ef9cc93e9d0d283a09ec4e3
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-09-19 12:21:04 +0430

    Added a dynamic url.

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 
-def index(request):
-    return HttpResponse("work 9 hours every day!")
-
-
-def february(request):
-    return HttpResponse("run 45 mins every day!")
+def monthly_challenges(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Eat more fruits!"
+    elif month == "february":
+        challenge_text = "Walk!"
+    elif month == "march":
+        challenge_text = "Read!"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
