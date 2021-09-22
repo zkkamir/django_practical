470735b96952addc5e3f140e5db0306b743b7a81
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-09-22 21:58:49 +0330

    Added str and full name method to author.

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Author
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -10,3 +10,4 @@ class BookAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
