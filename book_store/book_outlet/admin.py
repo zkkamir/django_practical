40d501296fd6510ad1fa7f52f122938d8c22f1c7
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-09-22 15:54:20 +0330

    Tweaked the admin pannel.

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -5,6 +5,8 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title", )}
+    list_filter = ("author", "rating")
+    list_display = ("title", "author")
 
 
 admin.site.register(Book, BookAdmin)
