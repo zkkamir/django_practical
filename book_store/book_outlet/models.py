0d6b88f0f2cab439f5faaa0ca7b6274f6dc25e64
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-09-22 11:56:34 +0330

    Added str method for Book model.

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=255)
     rating = models.IntegerField()
+
+    def __str__(self):
+        return self.title
