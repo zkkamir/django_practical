69cc7d323838bb148d38778e35a0203725ac4409
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-09-22 13:56:13 +0330

    overwrote the get_absolute url method.

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+from django.urls import reverse
+
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
@@ -8,6 +10,9 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(max_length=255, null=True)
     is_bestselling = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.id])
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
