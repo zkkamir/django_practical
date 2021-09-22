1f5c53f356f857e0e83834509e70a33c16bd6e5b
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-09-22 14:16:23 +0330

    Added slug field and overwritten the save method.

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Book(models.Model):
@@ -10,9 +11,14 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(max_length=255, null=True)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False)
 
     def get_absolute_url(self):
-        return reverse("book-detail", args=[self.id])
+        return reverse("book-detail", args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
