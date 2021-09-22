b9df8a9c04d7dbc2156d7b99d3c49bec9d7918b8
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-09-22 15:22:55 +0330

    Used aggregate funcs and order_by.

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
+from django.db.models import Avg
 
 from .models import Book
 
 
 def index(request):
-    all_books = Book.objects.all()
+    all_books = Book.objects.all().order_by("-rating")
+    tnob = all_books.count()
+    average_rating = all_books.aggregate(Avg("rating"))
+
     return render(request, "book_outlet/index.html", context={
-        "all_books": all_books
+        "all_books": all_books,
+        "total_number_of_books": tnob,
+        "average_rating": average_rating
     })
 
 
@@ -18,5 +24,5 @@ def book_detail(request, slug):
     #     raise Http404
     book = get_object_or_404(Book, slug=slug)
     return render(request, "book_outlet/book-detail.html", context={
-        "book": book
+        "book": book,
     })
