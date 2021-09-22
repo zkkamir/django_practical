7abd379361393a07e9b3fa39f808b114d78774a1
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-09-22 15:48:56 +0330

    Added book admin manager.

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -16,9 +16,5 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
-
     def __str__(self):
         return f"{self.title} ({self.rating})"
