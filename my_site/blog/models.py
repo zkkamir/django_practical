556aa77b532a45d0fe0ca1430f319373bf9a4090
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-09-23 10:24:15 +0330

    Added models for blog posts.

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    """A class to represent post authors."""
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+
+
+class Tag(models.Model):
+    """A class to represent tags of blog posts."""
+    caption = models.CharField(max_length=255)
+
+
+class Post(models.Model):
+    """A class to represent blog posts."""
+    title = models.CharField(max_length=255)
+    excerpt = models.CharField(max_length=255)
+    content = models.TextField()
+    slug = models.SlugField(db_index=True)
+    date = models.DateField()
+    image = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
