6bb2c23787fb797c070ca211bd81e76447c98cee
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-09-23 10:32:33 +0330

    Added the helper methods to the models.

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Author(models.Model):
@@ -7,10 +8,22 @@ class Author(models.Model):
     last_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
 
+    def get_full_name(self):
+        """Return the full name of the author."""
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        """Return the string representation of the model."""
+        return self.get_full_name()
+
 
 class Tag(models.Model):
     """A class to represent tags of blog posts."""
     caption = models.CharField(max_length=255)
+
+    def __str__(self):
+        """Return the string representation of the model."""
+        return self.caption
 
 
 class Post(models.Model):
@@ -23,3 +36,11 @@ class Post(models.Model):
     image = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
+
+    def get_absolute_url(self):
+        """Return the absolute url of a blog post."""
+        return reverse("post-detail", args=[self.slug])
+
+    def __str__(self):
+        """Return the string representation of the model."""
+        return self.title
