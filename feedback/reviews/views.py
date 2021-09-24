1e0e0c7e7b5fbcc536bddf799df34cbcde7e9665
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-09-24 13:17:22 +0330

    Used CBVs.

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,26 +1,28 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
 
 from .forms import ReviewForm
 
 
-def review(request):
-    if request.method == "POST":
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            # review = Review(
-            #     username=form.cleaned_data["username"],
-            #     review_text=form.cleaned_data["review_text"],
-            #     rating=form.cleaned_data["rating"])
-            # review.save()
-            return HttpResponseRedirect("/thank-you")
-    else:
+class ReviewView(View):
+    def get(self, request):
         form = ReviewForm()
 
-    return render(request, "reviews/review.html", {
-        "form": form
-    })
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+
+    def post(self, request):
+        form = ReviewForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/thank-you")
+
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
 
 
 def thank_you(request):
