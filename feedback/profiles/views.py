2d6ad84901aaf0078aa4f6be20871b296c2d4449
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-09-24 17:58:25 +0330

    Used a model and form to upload the form.

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -2,17 +2,25 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 
-
-def store_file(file):
-    with open("temp/image.jpg", "wb+") as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
+from .forms import ProfileForm
+from .models import UserProfile
 
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            "form": form
+        })
 
     def post(self, request):
-        store_file(request.FILES["image"])
-        return HttpResponseRedirect("/profiles")
+        submitted_form = ProfileForm(request.POST, request.FILES)
+
+        if submitted_form.is_valid():
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
+            return HttpResponseRedirect("/profiles")
+
+        return render(request, "profiles/create_profile.html", {
+            "form": submitted_form
+        })
