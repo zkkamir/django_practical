f997154dbefa9b4f61d12a6375383d7b784ddc86
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-09-24 16:51:56 +0330

    Used CreateView.

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,31 +1,18 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import DeleteView, ListView
+from django.views.generic.edit import CreateView
 
 from .forms import ReviewForm
 from .models import Review
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
-
-    def post(self, request):
-        form = ReviewForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "thank-you"
 
 
 class ThankYouView(TemplateView):
